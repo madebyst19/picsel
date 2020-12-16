00a703a457dbce217ce9a870fbb0e5e3ab7d8b97
sawaki <tomo8409t@gmail.com>
2020-12-16 09:34:21 +0900

    adjusted_indent

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,9 +2,7 @@ class Public::OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_search
   def update
-    # @order_detail = OrderDetail.find(params[:id])
     @order = Order.find(params[:id])
-    #  binding.pry
     if @order.update(status: order_params[:status].to_i)
         if params[:order][:status] == "1"
             @order.order_details.each do |order_detail|
@@ -21,82 +19,56 @@ class Public::OrdersController < ApplicationController
     # Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
       @orders = Order.where(user_id:current_user.id)
       cart_items = CartItem.where(user_id:current_user.id)
-      #@users = User.where(id:@orders.user_id)
   end
-def show
-  # binding.pry
-    @order = Order.find(params[:id])
-    @order_payment = "クレジット"
-    @order_details = OrderDetail.where(order_id: @order.id)
-end
-
-
-def edit
-end
-
-# @order = Order.find(params[:id])
-# @order_details = OrderDetail.where(order_id: @order.id)
-# @cart_items = current_user.cart_items
-# @sum = 0
-# @order.order_details.each do |order_detail|
-# @sum = @sum + (order_detail.item.price*1.08*order_detail.amount).round
-# end
-# @sum
-
-
-def destroy
-end
-
-# def current_user
-#   User.find(session[:user_id])
-# end
-
-def new
-  @order = Order.new
-end
-def create
-  @order = current_user.orders.new(order_create_params)
-  @order.status = 0
-  @order.order_details.each do |order_detail|
-    order_detail.shipping_status = 0
-    order_detail.save
+  def show
+      @order = Order.find(params[:id])
+      @order_payment = "クレジット"
+      @order_details = OrderDetail.where(order_id: @order.id)
   end
-  @order.buyer_id = current_user.id
-  if @order.save
-    @cart_items = current_user.cart_items
-    @cart_items.each do |cart_item|
-      @order_details = @order.order_details.new
-      @order_details.photo_id = cart_item.photo.id
-      @order_details.price = cart_item.photo.price * 1.08
-      @order_details.amount = cart_item.amount
-      @order_details.shipping_status = 0
-      @order_details.save
+  def new
+    @order = Order.new
+  end
+  def create
+    @order = current_user.orders.new(order_create_params)
+    @order.status = 0
+    @order.order_details.each do |order_detail|
+      order_detail.shipping_status = 0
+      order_detail.save
     end
-    current_user.cart_items.destroy_all
-    redirect_to  complete_public_orders_path
-         else
-            render "confirm"
-    end
-end
-  def confirm
-      if params['payjp-token'] == ''
-        flash[:notice] = 'payjp token is empty'
-        @order = current_user.orders.new(order_params)
-        return render :new
-        # redirect_to  complete_public_orders_path
-      end
-      @order = current_user.orders.new
-      @order_postal_code = params[:order][:postal_code]
+    @order.buyer_id = current_user.id
+    if @order.save
       @cart_items = current_user.cart_items
-      CartItem.where(user_id: current_user.id)
-      @order_payment = params[:order][:payment_method]
-      #@order_payment = 'credit'
-      @address_option = params[:order][:address_option]
-      if @address_option == "0"
-          @order_address = Order.new(address: current_user.address,postal_code: current_user.postal_code,name: current_user.first_name + current_user.last_name)
-      elsif @address_option == "1"
-          @order_address = Order.new(order_params)
+      @cart_items.each do |cart_item|
+        @order_details = @order.order_details.new
+        @order_details.photo_id = cart_item.photo.id
+        @order_details.price = cart_item.photo.price * 1.08
+        @order_details.amount = cart_item.amount
+        @order_details.shipping_status = 0
+        @order_details.save
       end
+      current_user.cart_items.destroy_all
+      redirect_to  complete_public_orders_path
+    else
+      render "confirm"
+    end
+  end
+  def confirm
+    if params['payjp-token'] == ''
+      flash[:notice] = 'payjp token is empty'
+      @order = current_user.orders.new(order_params)
+      return render :new
+    end
+    @order = current_user.orders.new
+    @order_postal_code = params[:order][:postal_code]
+    @cart_items = current_user.cart_items
+    CartItem.where(user_id: current_user.id)
+    @order_payment = params[:order][:payment_method]
+    @address_option = params[:order][:address_option]
+    if @address_option == "0"
+        @order_address = Order.new(address: current_user.address,postal_code: current_user.postal_code,name: current_user.first_name + current_user.last_name)
+    elsif @address_option == "1"
+        @order_address = Order.new(order_params)
+    end
   end
   def sold
     user = User.find(params[:id])
@@ -106,13 +78,12 @@ end
     def sold_detail
       @order = Order.find(params[:id])
       @order_details = @order.order_details.where(photo_id:current_user.photos.pluck(:id))
-      # @order_payment = params[:order][:payment_method]
   end
   private
   def order_params
-      params.require(:order).permit(:cart_item_id,:postal_code,:address,:name,:payment_method,:status,:total_payment)
-    end
-    def order_create_params
-      params.require(:order).permit(:postal_code,:address,:name,:payment_method,:total_payment,:buyer_id)
-    end
+    params.require(:order).permit(:cart_item_id,:postal_code,:address,:name,:payment_method,:status,:total_payment)
+  end
+  def order_create_params
+    params.require(:order).permit(:postal_code,:address,:name,:payment_method,:total_payment,:buyer_id)
+  end
 end
--- app/controllers/public/photos_controller.rb
+++ app/controllers/public/photos_controller.rb
@@ -1,22 +1,15 @@
 class Public::PhotosController < ApplicationController
     before_action :authenticate_user!
     def index
-        # binding/pry
         @tag_list = Tag.all  
         if params[:user_id].nil?
             @user = current_user
-            # @photos = Photo.where(user_id: current_user.id)
         else
-            # binding.pry
             @user = User.find(params[:user_id])
         end
         @photos = Photo.where(user_id: @user.id)
-        #  binding.pry
-        # @Photoss = Photo.search(params[:search])
         @hashtag = Hashtag.find_by(hashname: params[:tag])
         @hashtag_photos = @hashtag.photos if @hashtag.present?
-        # @hashtag_photos = HashtagPhoto.where(hashtag_id: @hashtag.id)
-        # @hashtag_photos = HashtagPhoto.search(params[:search])
     end
     def new
         @photo = Photo.new
@@ -25,35 +18,22 @@ class Public::PhotosController < ApplicationController
     end
     def create
         @photo = current_user.photos.new(photo_params)
-        # tag_list = params[:photo][:tag_name].split(nil)
-        # if @photo.save
-        #     @photo.save_tag(tag_list)
-        #     redirect_back(fallback_location: root_path)
-        # else
-        #     redirect_back(fallback_location: root_path)
-        # end
-
         if photo_params[:is_active] == '有効'
             @photo.is_active = true
         elsif photo_params[:is_active] == '無効'
             @photo.is_active = false
         end
-
         if @photo.save
             redirect_to public_photo_path(@photo.id)
-            # redirect_to users_photo_path(@photo.id)
         else
             render new_public_photo_path
         end
-
     end
     def show
         @photo = Photo.find(params[:id])
         @cart_item = CartItem.new
         @user = User.find_by(id: @photo.user_id)
         @like = Like.new
-        # @photo_tags = @photo.tags
-        # @tag = Tag.find_by(id:@photo.tag_id)
     end
     def edit
         @photo = Photo.find(params[:id])
@@ -63,9 +43,9 @@ class Public::PhotosController < ApplicationController
         @photo = Photo.find(params[:id])
         if @photo.update(photo_params)
             redirect_to public_photo_path(@photo)
-          else
+        else
             render "index"
-          end
+        end
     end
     def destroy
         @photo = Photo.find(params[:id])
@@ -73,22 +53,14 @@ class Public::PhotosController < ApplicationController
        redirect_to public_photos_path
     end
     def hashtag
-        # @q = Hashtag.find(hashname: params[@q])
-        #  binding.pry
         @hashname =  params[:hashname_cont]
-        # @hashtag = Hashtag.find_by(hashname: @hashname)
         @hashtags = Hashtag.where(['hashname LIKE ?', "%#{params[:hashname_cont]}%"])
         hashtag_photo_ids = []
         @hashtags.each do |hashtag|
-            # binding.pry
             hashtag.hashtag_photos.each do |hashtag_photo|
                 hashtag_photo_ids << hashtag_photo.id
-            # binding.pry
             end
         end
-        # @hashtag_photos = if @hashtag.present?
-            #   @photo = @hashtag.photo.page(params[:page]).per(20).reverse_order
-        #  binding.pry
         if @hashname == ""
             @hashtag_photos =  []
             @hashtag_photos_count = 0
@@ -96,18 +68,12 @@ class Public::PhotosController < ApplicationController
             @hashtag_photos = HashtagPhoto.where(id: hashtag_photo_ids.uniq)
             @hashtag_photos_count = @hashtag_photos.count
         end
-        # else
-        # end
-        # binding.pry
-        # @hashtags = Hashtag.all.to_a.group_by{ |hashtag| hashtag.photos.count}
       end
       def search
-        #Viewのformで取得したパラメータをモデルに渡す
         @Photoss = Photo.search(params[:search])
       end
-
     private
-        def photo_params
-            params.require(:photo).permit(:title, :gallary_id , :image, :caption, :price,:hashbody, :is_active, hashtag_ids:[])
-        end
+    def photo_params
+        params.require(:photo).permit(:title, :gallary_id , :image, :caption, :price,:hashbody, :is_active, hashtag_ids:[])
+    end
 end
